28dd27077a9bcfecfd72e93827b88ae9ebf4bb87
Sarah <vanwars@gmail.com>
2018-03-03 16:45:56 -0800

    Second commit

---
+++ classwork/0301/node-practice/addition.js
@@ -0,0 +1,16 @@
+/*module.exports.sum = (a, b) => {
+    return a + b
+}
+
+module.exports.difference = (a, b) => {
+    return a - b
+}*/
+
+module.exports = {
+    sum: (a, b) => {
+        return a + b
+    },
+    difference: (a, b) => {
+        return a - b
+    }
+}
---
+++ classwork/0301/node-practice/math-stuff.js
@@ -0,0 +1,6 @@
+const mathFun = require('./addition.js')
+const cat = require('cat-me')
+console.log(cat('grumpy'))
+console.log(cat('thoughtful'))
+console.log(mathFun.sum(2, 2))
+console.log(mathFun.difference(2, 2))
