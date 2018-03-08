5b74a3a9bd52a730aa0800dbacdb6b5caccc15a3
Sarah <vanwars@gmail.com>
2018-03-08 11:50:01 -0800

    added post form

--- assignment2/client/main.js
+++ assignment2/client/main.js
@@ -1,21 +1,31 @@
-fetch('http://localhost:3000/posts/').then(response =>{
-    return response.json();
-  })
-  .then(posts => {
-      console.log(posts)
-      const container = document.getElementById('container')
-      posts.forEach(post => {
-          let template = `
-                <h2>${post.name}</h2>
-                <a href="${post.url}">More</a>
-                <p>${post.text}</p>
-                <h3>Comments:</h3>
-                <ul>
-            `
-          post.comments.forEach(comment => {
-              template += `<li>${comment.text}</li>`
-          })
-          template += '</ul>'
-          container.innerHTML += template;
+//1. define functions:
+const getPosts = () => {
+    fetch('http://localhost:3000/posts/').then(response =>{
+        return response.json();
       })
-  });
+      .then(posts => {
+          console.log(posts)
+          const container = document.getElementById('container')
+          posts.forEach(post => {
+              let template = `
+                    <h2>${post.name}</h2>
+                    <a href="${post.url}">More</a>
+                    <p>${post.text}</p>
+                    <h3>Comments:</h3>
+                    <div class="comments">TODO next week...</div>`
+              container.innerHTML += template;
+          })
+    })
+}
+
+const attachEventHandlers = () => {
+    document.querySelectorAll('.close, button').forEach(elem => {
+        elem.onclick = () => {
+            document.querySelector('.modal').classList.toggle('show')
+        }
+    })
+}
+
+//2. call functions:
+attachEventHandlers()
+getPosts()
