d305e091be97b4bab0436a58f72e40606c9ba8c3
Sarah <vanwars@gmail.com>
2018-03-08 17:51:34 -0800

    minor edits

--- assignment007/client/js/getPosts.js
+++ assignment007/client/js/getPosts.js
@@ -3,19 +3,22 @@ const getPosts = () => {
     container.innerHTML = ''
     fetch('http://localhost:3000/posts/').then(response =>{
         return response.json();
-      })
-      .then(posts => {
-          console.log(posts)
-          const container = document.getElementById('container')
-          posts.forEach(post => {
-              let template = `
-                    <h2>${post.name}</h2>
-                    <a href="${post.url}">More</a>
-                    <p>${post.text}</p>
-                    <h3>Comments:</h3>
-                    <div class="comments">TODO next week...</div>`
-              container.innerHTML += template;
-          })
+      }).then(showPosts)
+}
+
+const showPosts = (posts) => {
+    console.log(posts)
+    const container = document.getElementById('container')
+    posts.forEach(post => {
+        let template = `
+              <h2>${post.name}</h2>
+              <a href="${post.url}">More</a>
+              <p>${post.text}</p>
+              <div class="comments">
+                    <h3>Comments</h3>
+                    <p>TODO next week...</p>
+              </div>`
+        container.innerHTML += template;
     })
 }
 getPosts()
--- assignment007/server/app.js
+++ assignment007/server/app.js
@@ -9,7 +9,7 @@ let store = {
     posts: [{
         name: 'What is going on right now in this EdX course?',
         url: 'https://courses.edx.org/courses/course-v1:Microsoft+DEV283x+2T2017/course/',
-        text: 'You are learning to use developer documentation and resources.',
+        text: 'As we have already discussed, this course is a little advanced and some of the language and concepts are glossed over because the teacher already assumes basic fluency with server-side programming. That is OK. You will still learn a ton, even if you don\'t understand everything. You are learning to use developer documentation and resources, and we will go over the jargon and concepts in class.',
         comments: [{
                 text: 'So many assumptions are being made about what we already know?! This is overwhelming!'
             }, {
