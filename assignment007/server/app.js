2dc04d6fdb0ebe27797c888b2c64799773c89c0b
Sarah <vanwars@gmail.com>
2018-03-08 19:17:00 -0800

    updates

--- assignment007/server/app.js
+++ assignment007/server/app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/posts', routes.posts.getPosts)
+app.get('/posts/:postId', routes.posts.getPost)
 app.post('/posts', routes.posts.addPost)
 app.put('/posts/:postId', routes.posts.updatePost)
 app.delete('/posts/:postId', routes.posts.removePost)
--- assignment007/server/routes/posts.js
+++ assignment007/server/routes/posts.js
@@ -5,17 +5,25 @@ module.exports = {
         console.log('\n\n\n\n')
         res.status(200).send(req.store.posts)
     },
+    getPost(req, res) {
+        const postID = req.params.postId
+        if (postID >= req.store.posts.length) {
+            res.status(500).send({error: `${postID} is out of range`})
+        } else {
+            res.status(200).send(req.store.posts[postID])
+        }
+    },
     addPost(req, res) {
         console.log(req.body)
         let newPost = req.body
-        let postId = req.store.posts.length
+        let postID = req.store.posts.length
         req.store.posts.push(newPost)
-        res.status(201).send({postId: postId})
+        res.status(201).send({postId: postID})
     },
     updatePost(req, res) {
         const postID = req.params.postId
         req.store.posts[postID] = Object.assign(req.store.posts[postID], req.body)
-        res.status(200).send(req.store.posts[req.params.postId])
+        res.status(200).send(req.store.posts[postID])
     },
     removePost(req, res) {
         const postID = req.params.postId
